04d18ea598efc904b8f068f2432e51d2be3ec997
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2020-01-14 20:34:31 +0100

    repo: added initial version of script.

---
+++ convert_libretro_packages.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+ROOT="$(git rev-parse --show-toplevel)"
+
+LIBRETRO_CORES_SH_DIR="${ROOT}/retropie/scriptmodules/libretrocores"
+LIBRETRO_CORES_BB_DIR="${ROOT}/results"
+
+WORKDIR="${ROOT}/workdir"
+mkdir -p ${WORKDIR}
+
+function append_line() {
+	local file=${1}
+	local line=${2}
+	echo -e "${line}" >> ${file}
+}
+
+pushd ${LIBRETRO_CORES_SH_DIR}
+	for file in `find . -iname \*.sh`; do
+		source ${file}
+		name=${file##*lr-}
+		name=${name%*.sh}
+		license_url=${rp_module_licence##* }
+		
+		repo=`grep ${file} -e 'gitPullOrClone'`
+		repo="${repo##*https://}"
+		echo ${repo}
+		
+		license_file="${WORKDIR}/license-${name}"
+		[ ! -f ${license_file} ] && wget -O "" "${license_url}"
+		license_md5=`md5sum ${license_file}`
+		license_md5=${license_md5%% *}
+		
+		[[ -z ${repo} ]] && continue
+		
+
+		mkdir -p ${LIBRETRO_CORES_BB_DIR}/${name}-libretro
+		recipe="${LIBRETRO_CORES_BB_DIR}/${name}-libretro/${name}-libretro.bb"
+		echo > ${recipe}
+		append_line ${recipe} "DESCRIPTION = \"${rp_module_desc}\"\n"
+		append_line ${recipe} "LICENSE = \"${rp_module_licence%% *}\""
+		append_line ${recipe} "LIC_FILES_CHKSUM = \"file://${rp_module_licence##*/};md5sum=${license_md5}\"\n"
+		append_line ${recipe} "PR = \"r1\"\n"
+		append_line ${recipe} "inherit libretro\n"
+		append_line ${recipe} "S = \"\${WORKDIR}/git\""
+		append_line ${recipe} "SRC_URI = \"gitsm://${repo};protocol=https\""
+		append_line ${recipe} "SRCREV = \"\${AUTOREV}\""
+		
+		
+	done
+popd
+
+
