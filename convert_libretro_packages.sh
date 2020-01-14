c803f4598beb562b6df6926927385b9fb43b2d8d
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2020-01-14 22:39:15 +0100

    script: fixed md5 sum parameter, added orginal file as tmp.

--- convert_libretro_packages.sh
+++ convert_libretro_packages.sh
@@ -21,24 +21,28 @@ pushd ${LIBRETRO_CORES_SH_DIR}
 		name=${name%*.sh}
 		license_url=${rp_module_licence##* }
 
+		cp -f "${file}" "${LIBRETRO_CORES_BB_DIR}/${name}-libretro/orginal.tmp"
+
 		repo=`grep ${file} -e 'gitPullOrClone'`
 		repo="${repo##*https://}"
 		repo="${repo%% *}"
-		echo ${repo}
 
 		license_file="${WORKDIR}/license-${name}"
 		[ ! -f ${license_file} ] && wget -O "" "${license_url}"
 		license_md5=`md5sum ${license_file}`
 		license_md5=${license_md5%% *}
 
-		[[ -z ${repo} ]] && continue
+		if [[ -z ${repo} ]]; then
+			echo "${name}: unable to find repo information"
+			repo="unknown"
+		fi
 
 		mkdir -p ${LIBRETRO_CORES_BB_DIR}/${name}-libretro
 		recipe="${LIBRETRO_CORES_BB_DIR}/${name}-libretro/${name}-libretro.bb"
 		echo -n > ${recipe}
 		append_line ${recipe} "DESCRIPTION = \"${rp_module_desc}\"\n"
 		append_line ${recipe} "LICENSE = \"${rp_module_licence%% *}\""
-		append_line ${recipe} "LIC_FILES_CHKSUM = \"file://${rp_module_licence##*/};md5sum=${license_md5}\"\n"
+		append_line ${recipe} "LIC_FILES_CHKSUM = \"file://${rp_module_licence##*/};md5=${license_md5}\"\n"
 		append_line ${recipe} "PR = \"r1\"\n"
 		append_line ${recipe} "inherit libretro\n"
 		append_line ${recipe} "S = \"\${WORKDIR}/git\""
