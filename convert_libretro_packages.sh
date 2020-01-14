9e136d762fb2d9ba206a467a86f2ef547758eb72
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2020-01-14 23:07:05 +0100

    script: added libretro core detection.

--- convert_libretro_packages.sh
+++ convert_libretro_packages.sh
@@ -23,14 +23,26 @@ pushd ${LIBRETRO_CORES_SH_DIR}
 
 		cp -f "${file}" "${LIBRETRO_CORES_BB_DIR}/${name}-libretro/orginal.tmp"
 
+		unset repo
+		unset core
+
 		repo=`grep ${file} -e 'gitPullOrClone'`
 		repo="${repo##*https://}"
 		repo="${repo%% *}"
+
+		core=`grep ${file} -e "_libretro.so'"`
+		core=${core//\'}
+		core=${core// }
 
 		license_file="${WORKDIR}/license-${name}"
 		[ ! -f ${license_file} ] && wget -O "" "${license_url}"
 		license_md5=`md5sum ${license_file}`
 		license_md5=${license_md5%% *}
+
+		if [[ -z ${core} ]]; then
+			echo "${name}: unable to find core"
+			core="unknown"
+		fi
 
 		if [[ -z ${repo} ]]; then
 			echo "${name}: unable to find repo information"
@@ -43,11 +55,11 @@ pushd ${LIBRETRO_CORES_SH_DIR}
 		append_line ${recipe} "DESCRIPTION = \"${rp_module_desc}\"\n"
 		append_line ${recipe} "LICENSE = \"${rp_module_licence%% *}\""
 		append_line ${recipe} "LIC_FILES_CHKSUM = \"file://${rp_module_licence##*/};md5=${license_md5}\"\n"
-		append_line ${recipe} "PR = \"r1\"\n"
 		append_line ${recipe} "inherit libretro\n"
 		append_line ${recipe} "S = \"\${WORKDIR}/git\""
 		append_line ${recipe} "SRC_URI = \"gitsm://${repo};protocol=https\""
-		append_line ${recipe} "SRCREV = \"\${AUTOREV}\""
+		append_line ${recipe} "SRCREV = \"\${AUTOREV}\"\n"
+		append_line ${recipe} "LIBRETRO_CORE = \"${core}\"\n"
 
 		sed -i "s/GPL2/GPLv2/g" ${recipe}
 		sed -i "s/GPL3/GPLv3/g" ${recipe}
